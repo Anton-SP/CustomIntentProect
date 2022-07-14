84fae0b18261f985e58fbfcc57dfb60c5c482024
AntonPetrov <pasmobiledev@gmail.com>
2022-07-14 15:24:40 +0300

    Basic view and CustomIntent class

--- app/src/main/java/com/example/customintentproect/MainActivity.kt
+++ app/src/main/java/com/example/customintentproect/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.customintentproect
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.customintentproect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.mainActivityLoadButton.setOnClickListener() { view ->
+            Toast.makeText(this,"click",Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
+
+
+
+
